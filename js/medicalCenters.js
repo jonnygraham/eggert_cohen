23273eff56f58a3706494586432d945ff2a53329
unknown <jonnyg@illtjonnyg.nice.com>
2013-07-28 01:10:08 +0300

    test

---
+++ js/medicalCenters.js
@@ -0,0 +1,25 @@
+var medicalCenters = [{
+	area: 'Jerusalem',
+	name: 'ABC Medical',
+	location: { longitude: 32.814257, latitude: 35.76479 },
+	phone: '02-233-4244'
+	},
+	{
+	area: 'Jerusalem',
+	name: 'XYZ Medical',
+	location: { longitude: 32.76779, latitude: 35.689774 },
+	phone: '02-984-4444'
+	},
+	{
+	area: 'Tel Aviv',
+	name: 'skdjk Medical',
+	location: { longitude: 300, latitude: -400 },
+	phone: '02-233-4244'
+	},
+	{
+	area: 'TelAviv',
+	name: 'oooo Medical',
+	location: { longitude: 323, latitude: -403 },
+	phone: '02-984-4444'
+	}]
+	
