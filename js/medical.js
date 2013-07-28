9749e106eb51a07a3697dccccaf29d89762351fe
unknown <jonnyg@illtjonnyg.nice.com>
2013-07-29 00:28:47 +0300

    test

--- js/medical.js
+++ js/medical.js
@@ -1,29 +1,71 @@
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d.toFixed(2);
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 function initialize() {
-  var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(32.78605,35.693872),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-  new google.maps.Marker({
-					position: map.center,
-					map: map,
-					title: 'You are here'
-				});
+	if (navigator.geolocation)
+    {
+		navigator.geolocation.getCurrentPosition(showCurrentPosOnMap,handleNoGeo);
+    }
+	else {
+		handleNoGeo();
+	}
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+function prepareMedicalCenters() {
+	medicalCentersList = new Array()
+	$.each(medicalCenters, function(idx, obj){
+		var distance = getDistanceFromLatLonInKm(myPos.lat(), myPos.lng(), obj.location.latitude, obj.location.longitude);
+		var latLng = new google.maps.LatLng(obj.location.latitude,obj.location.longitude);
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: map,
+			title: obj.name
+		});
+		var infowindow = new google.maps.InfoWindow({
+			content: '<div>'+obj.name+'</div>'
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			if (typeof openInfoWindow !== 'undefined') openInfoWindow.close();
+			openInfoWindow = infowindow;
+			infowindow.open(map,marker);
+		});
+		medicalCentersList[idx] = { distance: distance,
+			marker: marker,
+			medicalCenter: obj }
+	});
+}
+
 $(document).ready(function() {
-	$("#area").text("hello");
+	
+	
 	$("#areaSelector").change(function () {
 		var str = "";
 		var area = $("#areaSelector option:selected").val();
 		$("#area").text("Medical Centers in the "+area + " Area");
-		$.each(medicalCenters, function(idx, obj){ 
-			if(obj.area === area) {
-				str +="<h2>"+obj.name+"</h2>"
+		medicalCentersList.sort(function(a,b) {
+			return (a.distance < b.distance) ? -1 : 1
+		});
+		$.each(medicalCentersList, function(idx, obj){ 
+			if(obj.medicalCenter.area === area) {
+				str += displayMedicalCenter(obj) 
+				/*str +="<h2>"+obj.name+"</h2>"
 				str +="<ul>"
 				str +="</ul>"
 				$.each(obj, function(key, value){
@@ -31,13 +73,7 @@ $(document).ready(function() {
 						if (key === 'phone') value = "<a href='tel:"+value.replace(/-/g,'')+"'>"+value+"</a>"
 						str +="<li>"+key+": "+value+"</li>"
 					}
-				});
-				var medicalCenterPosition = new google.maps.LatLng(obj.location.longitude,obj.location.latitude);
-				new google.maps.Marker({
-					position: medicalCenterPosition,
-					map: map,
-					title: obj.name
-				});
+				});*/
 			}
 		});
 		$("#area").html(str)
@@ -45,12 +81,78 @@ $(document).ready(function() {
 	 //.trigger('change');
 })
 
-function show_map(loc) {
-  $("#map-container").css({'width':'320px','height':'350px'});
-  var map = new GMap2(document.getElementById("map-container"));
-  var center = new GLatLng(loc.coords.latitude, loc.coords.longitude);
-  map.setCenter(center, 14);
-  map.addControl(new GSmallMapControl());
-  map.addControl(new GMapTypeControl());
-  map.addOverlay(new GMarker(center, {draggable: false, title: "You are here (more or less)"}));
+function parsePhoneNumber(phoneNum) {
+	return phoneNum.replace(/-/g,'')
 }
+function displayMedicalCenter(obj) {
+
+	var lat = obj.marker.getPosition().lat()
+	var lng = obj.marker.getPosition().lng()
+	
+	var str ="<h2><a href='javascript:jumpToPosition("+lat+","+lng+")'>"+obj.medicalCenter.name+"</a></h2>"
+	str +="<ul>"
+	str +="<li>"+obj.distance+"km away</li>"
+
+	str +="<li>Phone: <a href='tel:"+parsePhoneNumber(obj.medicalCenter.phone)+"'>"+obj.medicalCenter.phone+"</a></li>"
+	str +="<li>Opening hours: "+obj.medicalCenter.openingHours+"</li>"
+	str +="</ul>"
+	return str;
+}
+
+function jumpToPosition(latitude, longitude) {
+	map.setCenter(new google.maps.LatLng(latitude, longitude));
+}
+
+function showMap(latlng,markerTitle) {
+  var mapOptions = {
+    zoom: 14,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  var marker = new google.maps.Marker({
+					position: map.center,
+					map: map,
+					title: markerTitle
+				});
+	var infowindow = new google.maps.InfoWindow({
+		content: '<div>'+markerTitle+'</div>'
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+		if (typeof openInfoWindow !== 'undefined') openInfoWindow.close();
+		openInfoWindow = infowindow;
+		infowindow.open(map,marker);
+	});
+}
+
+function showCurrentPosOnMap(position) {
+	myPos = new google.maps.LatLng(position.coords.latitude ,position.coords.longitude);
+	showMap(myPos,"You are here");
+	prepareMedicalCenters();
+}
+
+function handleNoGeo() {
+	myPos = new google.maps.LatLng(31.780496,35.217254);
+	showMap(myPos,"Egert & Cohen Jerusalem Office");
+	prepareMedicalCenters();
+}
+
+function showError(error)
+  {
+  switch(error.code) 
+    {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.")
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.")
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.")
+      break;
+    }
+  }
--- js/medicalCenters.js
+++ js/medicalCenters.js
@@ -1,25 +1,33 @@
 var medicalCenters = [{
+	id: '1',
 	area: 'Jerusalem',
 	name: 'ABC Medical',
-	location: { longitude: 32.814257, latitude: 35.76479 },
+	location: { latitude: 31.780496, longitude: 35.227254 },
+	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
 	phone: '02-233-4244'
 	},
 	{
+	id: '2',
 	area: 'Jerusalem',
 	name: 'XYZ Medical',
-	location: { longitude: 32.76779, latitude: 35.689774 },
+	location: { latitude: 31.780486, longitude: 35.216254 },
+	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
 	phone: '02-984-4444'
 	},
 	{
+	id: '3',
 	area: 'Tel Aviv',
 	name: 'skdjk Medical',
-	location: { longitude: 300, latitude: -400 },
+	location: { latitude: 300, longitude: -400 },
+	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
 	phone: '02-233-4244'
 	},
 	{
-	area: 'TelAviv',
+	id: '4',
+	area: 'Tel Aviv',
 	name: 'oooo Medical',
-	location: { longitude: 323, latitude: -403 },
+	location: { latitude: 323, longitude: -403 },
+	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
 	phone: '02-984-4444'
 	}]
 	
