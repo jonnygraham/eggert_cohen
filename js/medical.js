f4fd786f00e34d36e0e4b2473a661566a106bfc5
unknown <jonnyg@illtjonnyg.nice.com>
2013-09-02 11:00:56 +0300

    updated

--- js/medical.js
+++ js/medical.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-function initialize() {
-  var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(32.78605,35.693872),
-=======
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
@@ -97,60 +91,10 @@ $(document).ready(function() {
     var mapOptions = {
     zoom: 18,
     center: new google.maps.LatLng(31.780496,35.217254),
->>>>>>> gh-pages
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-<<<<<<< HEAD
-  new google.maps.Marker({
-					position: map.center,
-					map: map,
-					title: 'You are here'
-				});
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$(document).ready(function() {
-	$("#area").text("hello");
-	$("#areaSelector").change(function () {
-		var str = "";
-		var area = $("#areaSelector option:selected").val();
-		$("#area").text("Medical Centers in the "+area + " Area");
-		$.each(medicalCenters, function(idx, obj){ 
-			if(obj.area === area) {
-				str +="<h2>"+obj.name+"</h2>"
-				str +="<ul>"
-				str +="</ul>"
-				$.each(obj, function(key, value){
-					if (key !== 'location' && key !== 'name') {
-						str +="<li>"+key+": "+value+"</li>"
-					}
-				});
-				var medicalCenterPosition = new google.maps.LatLng(obj.location.longitude,obj.location.latitude);
-				new google.maps.Marker({
-					position: medicalCenterPosition,
-					map: map,
-					title: obj.name
-				});
-			}
-		});
-		$("#area").html(str)
-	});
-	 //.trigger('change');
-})
-
-function show_map(loc) {
-  $("#map-container").css({'width':'320px','height':'350px'});
-  var map = new GMap2(document.getElementById("map-container"));
-  var center = new GLatLng(loc.coords.latitude, loc.coords.longitude);
-  map.setCenter(center, 14);
-  map.addControl(new GSmallMapControl());
-  map.addControl(new GMapTypeControl());
-  map.addOverlay(new GMarker(center, {draggable: false, title: "You are here (more or less)"}));
-}
-=======
 	prepareData();
 	populateAreaList();
 	
@@ -289,4 +233,3 @@ function showError(error)
       break;
     }
   }
->>>>>>> gh-pages
--- js/medicalCenters.js
+++ js/medicalCenters.js
@@ -1,37 +1,20 @@
 var medicalCenters = [{
 	area: 'Jerusalem',
 	name: 'ABC Medical',
-<<<<<<< HEAD
-	location: { longitude: 32.814257, latitude: 35.76479 },
-=======
 	location: { latitude: 31.780496, longitude: 35.227254 },
 	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
->>>>>>> gh-pages
 	phone: '02-233-4244'
 	},
 	{
 	area: 'Jerusalem',
 	name: 'XYZ Medical',
-<<<<<<< HEAD
-	location: { longitude: 32.76779, latitude: 35.689774 },
-=======
 	location: { latitude: 31.780486, longitude: 35.216254 },
 	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
->>>>>>> gh-pages
 	phone: '02-984-4444'
 	},
 	{
 	area: 'Tel Aviv',
 	name: 'skdjk Medical',
-<<<<<<< HEAD
-	location: { longitude: 300, latitude: -400 },
-	phone: '02-233-4244'
-	},
-	{
-	area: 'TelAviv',
-	name: 'oooo Medical',
-	location: { longitude: 323, latitude: -403 },
-=======
 	location: { latitude: 300, longitude: -400 },
 	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
 	phone: '02-233-4244'
@@ -41,7 +24,6 @@ var medicalCenters = [{
 	name: 'oooo Medical',
 	location: { latitude: 323, longitude: -403 },
 	openingHours: 'Sun-Thurs 08:00 - 17:00. Friday 08:30-13:30',
->>>>>>> gh-pages
 	phone: '02-984-4444'
 	}]
 	
